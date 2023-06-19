d7ac22616f291bf3609e0ab49a90259d801c9316
YewQianSheng <patrickyew2002@gmail.com>
2023-06-19 15:31:15 +0800

    in progress

--- week6/zodiac.php
+++ week6/zodiac.php
@@ -47,50 +47,51 @@
         $day = $_POST['day'];
         $month = $_POST['month'];
         $year = $_POST['year'];
+        if (checkdate(array_search($month, $months) + 1, $day, $year)) {
+            $zodiacs = array(
+                "Rat", "Cow", "Tiger", "Rabbit", "Dragon", "Snake", "Horse",
+                "Sheep", "Monkey", "Chicken", "Dog", "Pig"
+            );
 
-        $zodiacs = array(
-            "Rat", "Cow", "Tiger", "Rabbit", "Dragon", "Snake", "Horse",
-            "Sheep", "Monkey", "Chicken", "Dog", "Pig"
-        );
+            $calculate = ($year - 1900) % 12;
+            $zodiac = $zodiacs[$calculate];
 
-        $calculate = ($year - 1900) % 12;
-        $zodiac = $zodiacs[$calculate];
+            $month = array_search($month, $months) + 1;
+            $westernZodiac = "";
 
-        $month = array_search($month, $months) + 1;
-        $westernZodiac = "";
+            if (($month == 3 && $day >= 21) || ($month == 4 && $day <= 19)) {
+                $westernZodiac = "Aries";
+            } elseif (($month == 4 && $day >= 20) || ($month == 5 && $day <= 20)) {
+                $westernZodiac = "Taurus";
+            } elseif (($month == 5 && $day >= 21) || ($month == 6 && $day <= 20)) {
+                $westernZodiac = "Gemini";
+            } elseif (($month == 6 && $day >= 21) || ($month == 7 && $day <= 22)) {
+                $westernZodiac = "Cancer";
+            } elseif (($month == 7 && $day >= 23) || ($month == 8 && $day <= 22)) {
+                $westernZodiac = "Leo";
+            } elseif (($month == 8 && $day >= 23) || ($month == 9 && $day <= 22)) {
+                $westernZodiac = "Virgo";
+            } elseif (($month == 9 && $day >= 23) || ($month == 10 && $day <= 22)) {
+                $westernZodiac = "Libra";
+            } elseif (($month == 10 && $day >= 23) || ($month == 11 && $day <= 21)) {
+                $westernZodiac = "Scorpio";
+            } elseif (($month == 11 && $day >= 22) || ($month == 12 && $day <= 21)) {
+                $westernZodiac = "Sagittarius";
+            } elseif (($month == 12 && $day >= 22) || ($month == 1 && $day <= 19)) {
+                $westernZodiac = "Capricorn";
+            } elseif (($month == 1 && $day >= 20) || ($month == 2 && $day <= 18)) {
+                $westernZodiac = "Aquarius";
+            } elseif (($month == 2 && $day >= 19) || ($month == 3 && $day <= 20)) {
+                $westernZodiac = "Pisces";
+            }
 
-        if (($month == 3 && $day >= 21) || ($month == 4 && $day <= 19)) {
-            $westernZodiac = "Aries";
-        } elseif (($month == 4 && $day >= 20) || ($month == 5 && $day <= 20)) {
-            $westernZodiac = "Taurus";
-        } elseif (($month == 5 && $day >= 21) || ($month == 6 && $day <= 20)) {
-            $westernZodiac = "Gemini";
-        } elseif (($month == 6 && $day >= 21) || ($month == 7 && $day <= 22)) {
-            $westernZodiac = "Cancer";
-        } elseif (($month == 7 && $day >= 23) || ($month == 8 && $day <= 22)) {
-            $westernZodiac = "Leo";
-        } elseif (($month == 8 && $day >= 23) || ($month == 9 && $day <= 22)) {
-            $westernZodiac = "Virgo";
-        } elseif (($month == 9 && $day >= 23) || ($month == 10 && $day <= 22)) {
-            $westernZodiac = "Libra";
-        } elseif (($month == 10 && $day >= 23) || ($month == 11 && $day <= 21)) {
-            $westernZodiac = "Scorpio";
-        } elseif (($month == 11 && $day >= 22) || ($month == 12 && $day <= 21)) {
-            $westernZodiac = "Sagittarius";
-        } elseif (($month == 12 && $day >= 22) || ($month == 1 && $day <= 19)) {
-            $westernZodiac = "Capricorn";
-        } elseif (($month == 1 && $day >= 20) || ($month == 2 && $day <= 18)) {
-            $westernZodiac = "Aquarius";
-        } elseif (($month == 2 && $day >= 19) || ($month == 3 && $day <= 20)) {
-            $westernZodiac = "Pisces";
+            echo "<p>Your Birth is:  $day /" . $month . "/ $year</p>";
+            echo "<p>Your 12 Chinese zodiac is: $zodiac</p>";
+            echo "<p>Your Western zodiac is: $westernZodiac</p>";
+        } else {
+            echo "<p>Invalid date entered. Please check your input and try again.</p>";
         }
-
-        echo "<p>Your Birth is:  $day /" . $month . "/ $year</p>";
-        echo "<p>Your 12 Chinese zodiac is: $zodiac</p>";
-        echo "<p>Your Western zodiac is: $westernZodiac</p>";
     }
-
-
     ?>
 </body>
 
