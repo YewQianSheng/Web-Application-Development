8219ebd16fd7532b08c1808fd74a205236897bb9
YewQianSheng <patrickyew2002@gmail.com>
2023-07-01 18:59:16 +0800

    in prograss

--- project/index.php
+++ project/index.php
@@ -14,6 +14,23 @@
         include 'menu_nav.php';
         ?>
 
+        <div class="welcome py-5 text-left">
+            <div class="row">
+                <div class="col-lg-8 mx-auto">
+                    <h1 class="display-4">Welcome to Company Name</h1>
+                    <p class="lead">We provide innovative solutions for your business.</p>
+                    <a href="#contact" class="btn btn-primary btn-lg">Get in touch</a>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="container col-lg-auto text-left mx-auto bg-light">
+            <h2>About Us</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu odio et dolor gravida auctor. Mauris pellentesque, nunc nec interdum facilisis, ligula neque gravida nisi, ac suscipit tortor sem at nisl.</p>
+            <p>Nullam sed purus ullamcorper, tempus sapien eu, consequat dui. Proin pharetra urna et mi eleifend, a condimentum est venenatis. Aenean cursus elit in risus feugiat dignissim.</p>
+        </div>
+
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 
