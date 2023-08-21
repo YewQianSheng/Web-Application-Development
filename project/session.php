ba768155dd316aed823c53ca0b2b4a08630c8f26
YewQianSheng <patrickyew2002@gmail.com>
2023-08-21 10:27:03 +0800

    do the login homepage

--- project/session.php
+++ project/session.php
@@ -4,3 +4,10 @@ if (!isset($_SESSION["customer_id"])) {
     header('Location: login_page.php');
     exit();
 }
+$logout = isset($_GET['logout']) ? $_GET['logout'] : "";
+if ($logout == "true") {
+    session_unset();
+    session_destroy();
+    header("Location: homepage.php");
+    exit();
+}
